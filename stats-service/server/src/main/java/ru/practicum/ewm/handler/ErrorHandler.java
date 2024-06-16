70d87ba70447ea3e1959f9f271d612a6a89c88e6
DZaparov <dimaxiz1989@yandex.ru>
2024-06-16 13:53:58 +0300

    fix: add 400 bad request class

--- stats-service/server/src/main/java/ru/practicum/ewm/handler/ErrorHandler.java
+++ stats-service/server/src/main/java/ru/practicum/ewm/handler/ErrorHandler.java
@@ -1,7 +1,10 @@
 package ru.practicum.ewm.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +18,22 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST) //400
     public ApiError handleRequestDateException(final RequestDateException e) {
+        log.info("{} {}", HttpStatus.BAD_REQUEST, e.getMessage());
+        return ApiError.builder()
+                .errors(e.toString())
+                .message(e.getMessage())
+                .reason("Incorrectly made request.")
+                .status(HttpStatus.BAD_REQUEST.toString())
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+    }
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            ConstraintViolationException.class,
+            MissingServletRequestParameterException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //400
+    public ApiError handleValidationException(final Exception e) {
         log.info("{} {}", HttpStatus.BAD_REQUEST, e.getMessage());
         return ApiError.builder()
                 .errors(e.toString())
