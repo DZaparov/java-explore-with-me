43b7e6beff84aa4b5013cc909295d0fd2378a829
DZaparov <dimaxiz1989@yandex.ru>
2024-06-16 13:46:10 +0300

    fix: add 400 bad request class

--- main-service/src/main/java/ru/practicum/ewm/handler/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/ewm/handler/ErrorHandler.java
@@ -1,8 +1,11 @@
 package ru.practicum.ewm.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -42,7 +45,10 @@ public class ErrorHandler {
                 .build();
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            ConstraintViolationException.class,
+            MissingServletRequestParameterException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST) //400
     public ApiError handleValidationException(final Exception e) {
         log.info("{} {}", HttpStatus.BAD_REQUEST, e.getMessage());
